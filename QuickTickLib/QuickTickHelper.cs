ed6615a8259cc33a8299f2263bbb7c331050b1fc
Uight <uightuight@gmail.com>
2025-04-06 21:51:08 +0200

    #3 Add function to check if Platform supports QuickTick functions

--- QuickTickLib/QuickTickHelper.cs
+++ QuickTickLib/QuickTickHelper.cs
@@ -1,7 +1,26 @@
-﻿namespace QuickTickLib;
+﻿using System.Runtime.InteropServices;
+
+namespace QuickTickLib;
 
 internal static class QuickTickHelper
 {
     internal const uint NtCreateWaitCompletionPacketAccessRights = (uint)Win32Interop.TimerAccessMask.TIMER_MODIFY_STATE | (uint)Win32Interop.TimerAccessMask.TIMER_QUERY_STATE;
     internal const uint CreateWaitableTimerExWAccessRights = (uint)Win32Interop.TimerAccessMask.TIMER_MODIFY_STATE | (uint)Win32Interop.TimerAccessMask.SYNCHRONIZE;
+
+    internal static bool PlatformSupportsQuickTick()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return false;
+        }
+
+        var version = Environment.OSVersion.Version;
+
+        // Windows 10 / Server 2016+ (NT 10.0) should support all needed functions
+        if (version.Major >= 10)
+        {
+            return true;
+        }
+        return false;
+    }
 }
