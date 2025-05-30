c3c44df35e329d69d88b56d1bcbc8d7922f61f16
Uight <uightuight@gmail.com>
2025-05-30 17:20:56 +0200

    #8 Prepare .net framework 4.8 support

---
+++ QuickTestProjectNetFramework/Program.cs
@@ -0,0 +1,27 @@
+﻿using QuickTickLib;
+using System;
+using System.Threading;
+
+namespace QuickTestProjectNetFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using QuickTickTimer timer = new(1000.0 / 60.0);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            Thread.Sleep(1000); // Run for 5 seconds
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object? sender, QuickTickElapsedEventArgs e)
+        {
+            var diff = e.SignalTime - e.ScheduledTime;
+            Console.WriteLine($"Timer elapsed! Scheduled: {e.ScheduledTime:dd.MM.yyyy HH:mm:ss.fff}, Actual: {e.SignalTime:dd.MM.yyyy HH:mm:ss.fff} ms diff: {diff.TotalMilliseconds}");
+        }
+    }
+}
